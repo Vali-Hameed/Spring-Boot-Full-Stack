7bd9c5852c2471173cdfcaa64f9b0b8d5dab5e3b
Vali-Hameed <valihameed88@gmail.com>
2025-09-09 18:13:13 +0100

    created method to get files stored in a bucket

--- backend/src/main/java/com/FullStack/s3/S3Service.java
+++ backend/src/main/java/com/FullStack/s3/S3Service.java
@@ -1,9 +1,14 @@
 package com.FullStack.s3;
 
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
 
 @Service
 public class S3Service {
@@ -16,5 +21,18 @@ public class S3Service {
     public void putObject(String bucketName, String key, byte[] file) {
         PutObjectRequest objectRequest = PutObjectRequest.builder().bucket(bucketName).key(key).build();
         s3Client.putObject(objectRequest, RequestBody.fromBytes(file));
+    }
+    public byte[] getObject(String bucketName, String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(bucketName).key(key).build();
+         ResponseInputStream<GetObjectResponse> res = s3Client.getObject(getObjectRequest);
+        try {
+
+            return res.readAllBytes();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
