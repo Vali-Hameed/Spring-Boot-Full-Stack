d27ed72c205bf73296013c9de9f0a4579a1bc567
Vali-Hameed <valihameed88@gmail.com>
2025-09-10 17:46:01 +0100

    added profile image id to postgres database

--- backend/src/main/java/com/FullStack/customer/Customer.java
+++ backend/src/main/java/com/FullStack/customer/Customer.java
@@ -16,6 +16,10 @@ import java.util.Objects;
                 @UniqueConstraint(
                         name = "customer_email_unique",
                         columnNames = "email"
+                ),
+                @UniqueConstraint(
+                        name = "profile_image_id_unique",
+                        columnNames = "profileImageId"
                 )
         }
 )
@@ -55,6 +59,10 @@ public class Customer implements UserDetails {
             nullable = false
     )
     private String password;
+    @Column(
+            unique = true
+    )
+    private String profileImageId;
 
     public Customer() {
     }
@@ -71,6 +79,17 @@ public class Customer implements UserDetails {
         this.password = password;
         this.age = age;
         this.gender = gender;
+    }
+    public Customer(Integer id,
+                    String name,
+                    String email,
+                    String password,
+                    Integer age,
+                    Gender gender,
+                    String profileImageId) {
+        this(id, name, email, password, age, gender);
+        this.profileImageId = profileImageId;
+
     }
 
     public Customer(String name,
@@ -125,28 +144,12 @@ public class Customer implements UserDetails {
         this.gender = gender;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age) && gender == customer.gender;
+    public String getProfileImageId() {
+        return profileImageId;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, age, gender);
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", age=" + age +
-                ", gender=" + gender +
-                '}';
+    public void setProfileImageId(String profileImageId) {
+        this.profileImageId = profileImageId;
     }
 
     @Override
@@ -182,5 +185,30 @@ public class Customer implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age) && gender == customer.gender && Objects.equals(password, customer.password) && Objects.equals(profileImageId, customer.profileImageId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age, gender, password, profileImageId);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", password='" + password + '\'' +
+                ", profileImageId='" + profileImageId + '\'' +
+                '}';
     }
 }
